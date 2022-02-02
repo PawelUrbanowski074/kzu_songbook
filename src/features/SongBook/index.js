944826349c18e634fb4151cfb4afcdce956d309b
palel <32599663+palel@users.noreply.github.com>
2022-02-02 21:31:48 +0100

    exampleSongButton usage, SongList usage

--- src/features/SongBook/index.js
+++ src/features/SongBook/index.js
@@ -1,14 +1,26 @@
+import { useSelector } from "react-redux";
 import { Container } from "../../generalTypes/Container";
 import { Header } from "../../generalTypes/Header";
 import Section from "../../generalTypes/Section";
+import { ExampleSongButton } from "./ExampleSongButton";
 import Search from "./Search";
+import { SongsList } from "./SongsList";
 
-export const SongBook = () => (
-  <Container>
-    <Header>Śpiewnik utworów  - KZU</Header>
-    <Section
-      title="Wyszykiwarka"
-      body={<Search />}
-    />
-  </Container>
-);
+export const SongBook = () => {
+
+  return (
+    <Container>
+      <Header>Śpiewnik utworów  - KZU</Header>
+      <Section
+        title="Wyszykiwarka"
+        body={<Search />}
+        extraHeaderContent={<ExampleSongButton />}
+      />
+      <Section
+        title="Lista utworów"
+        // extraHeaderContent={<p>asd</p>}
+        body={<SongsList />}
+      />
+    </Container>
+  );
+}
