96317dbf5a9a60b8d266be9650c4444e5c99d5a3
palel <32599663+palel@users.noreply.github.com>
2022-02-03 00:25:19 +0100

    styles for song text

--- src/features/SongBook/SongsList/styled.js
+++ src/features/SongBook/SongsList/styled.js
@@ -13,11 +13,10 @@ export const Item = styled.li`
   border-bottom: solid 1px ${({ theme }) => theme.color.silver};
   display: flex;
   flex-direction: column;
-  /* align-items: center; */
   word-break: break-word; 
 
   ${({ hidden }) => hidden && css`
-      display: none;
+    display: none;
   `}
 `;
 
@@ -29,14 +28,17 @@ export const TrackBar = styled.div`
 `;
 
 export const Content = styled.span`
-  padding: 0px 15px 0 0;
+  padding: 0 15px 0 0;
   flex-grow: 1;
   font-size: 22px;
-  font-weight: bold;
+  font-weight: 600;
 
   ${({ done }) => done && css`
-      text-decoration: line-through;
+    text-decoration: line-through;
   `}  
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
+    font-size: 16px;
+  }
 `;
 
 export const Button = styled.button`
@@ -49,20 +51,24 @@ export const Button = styled.button`
   margin: 0 10px;
 
   ${({ toggleDone }) => toggleDone && css`
-      background-color: ${({ theme }) => theme.color.forestGreen};
+    background-color: ${({ theme }) => theme.color.forestGreen};
   `}
 
   ${({ toggleHiden }) => toggleHiden && css`
-      background-color: ${({ theme }) => theme.color.brown_pod};
+    background-color: ${({ theme }) => theme.color.brown_pod};
   `}
 
   &:hover {
-      filter: brightness(110%);
-      cursor: pointer;
+    filter: brightness(110%);
+    cursor: pointer;
   }
 
   &:active {
       filter: brightness(120%);
+  }
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
+    width: 20px;
+    height: 20px;
   }
 `;
 
@@ -72,39 +78,54 @@ export const StyledLink = styled.div`
   transition: 0.3s;
 
   &:hover {
-      filter: brightness(110%);
-      cursor: pointer;
+    filter: brightness(110%);
+    cursor: pointer;
   }
 
   &:active {
-      filter: brightness(120%);
+    filter: brightness(120%);
   }
 `;
 
 export const StyledArrow = styled(ArrowIcon)`
-    color: ${({ theme }) => theme.color.white};
-    width: auto;
-    transform: rotate(90deg);
-    transition: 0.3s;
-    
-    ${({ rotate }) => rotate && css`
-      transform: rotate(270deg);
-    `}
+  color: ${({ theme }) => theme.color.white};
+  width: auto;
+  transform: rotate(90deg);
+  transition: 0.3s;
+  
+  ${({ rotate }) => rotate && css`
+    transform: rotate(270deg);
+  `}
 
-    *:disabled & {
-      color: ${({ theme }) => theme.color.waterloo};
-    }
+  *:disabled & {
+    color: ${({ theme }) => theme.color.waterloo};
+  }
 
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileVerticalMax}){
-      height: 8px;
-    }
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileVerticalMax}){
+    height: 8px;
+  }
 `;
 
 export const SongText = styled.article`
   padding: 15px;
   border-top: solid 1px ${({ theme }) => theme.color.silver};
 
-  ${({ toggleHiden }) => toggleHiden && css`
-    display: none;
-  `}
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
+    padding: 5px;
+  }
+`;
+
+export const Verse = styled.p`
+  padding-bottom: 5px;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  grid-gap: 0 10px;
+`;
+export const LineOfText = styled.p`
+  margin: 0;
+  padding: 0;
+  line-height: 1.1;
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
+    font-size: 14px;
+  }
 `;
