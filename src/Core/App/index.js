b07fdcc4cd779adf2d8254a22049bca84cfe452b
palel <32599663+palel@users.noreply.github.com>
2022-02-02 21:31:08 +0100

    App with hashrouter usage

--- src/Core/App/index.js
+++ src/Core/App/index.js
@@ -2,13 +2,26 @@ import { SongBook } from "../../features/SongBook";
 import { Backposter } from "./Backposter";
 import { TopBar } from "./TopBar"
 
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { toSongs, toSong } from "../routes";
+
 export const App = () => {
   return (
-    <>
+    <HashRouter>
       <TopBar />
       <Backposter />
-      <SongBook />
-    </>
+      <Switch>
+        <Route path={toSong()}>
+          <p>jeszcze nie ma</p>
+        </Route>
+        <Route path={toSongs()}>
+          <SongBook />
+        </Route>
+        <Route path="/">
+          <Redirect to={toSongs()} />
+        </Route>
+      </Switch>
+    </HashRouter>
   );
 }
 
