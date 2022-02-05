c7de7ada5f2639535a586c0eacdabea1d373a9e6
palel <32599663+palel@users.noreply.github.com>
2022-02-05 18:58:11 +0100

    Button renamed to SongListButton, separated

--- src/features/SongBook/SongsList/styled.js
+++ src/features/SongBook/SongsList/styled.js
@@ -42,39 +42,6 @@ export const Content = styled.span`
   }
 `;
 
-export const Button = styled.button`
-  color: ${({ theme }) => theme.color.white};
-  width: 30px;
-  height: 30px;
-  border: none;
-  justify-self: center;
-  transition: 0.3s;
-  margin: 0 10px;
-  font-size: 18px;
-
-  ${({ toggleDone }) => toggleDone && css`
-    background-color: ${({ theme }) => theme.color.forestGreen};
-  `}
-
-  ${({ toggleHiden }) => toggleHiden && css`
-    background-color: ${({ theme }) => theme.color.brown_pod};
-  `}
-
-  &:hover {
-    filter: brightness(110%);
-    cursor: pointer;
-  }
-
-  &:active {
-      filter: brightness(120%);
-  }
-  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
-    width: 20px;
-    height: 20px;
-    font-size: 12px;
-  }
-`;
-
 export const StyledLink = styled(Link)`
   text-decoration: none;
   transition: 0.3s;
