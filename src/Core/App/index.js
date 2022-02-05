af19562a908580d7fead2e2baa87e0ee35132ed5
palel <32599663+palel@users.noreply.github.com>
2022-02-05 23:01:59 +0100

    GoUpButton usage

--- src/Core/App/index.js
+++ src/Core/App/index.js
@@ -5,12 +5,14 @@ import { TopBar } from "./TopBar"
 
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import { toSongs, toSong } from "../routes";
+import { GoUpButton } from "./GoUpButton";
 
 export const App = () => {
   return (
     <HashRouter>
       <TopBar />
       <Backposter />
+      {/* <GoUpButton /> */}
       <Switch>
         <Route path={toSong()}>
           <SongPage />
