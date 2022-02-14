27c438e9e6b05eecece0988b8994b77b07965366
palel <32599663+palel@users.noreply.github.com>
2022-02-14 23:26:14 +0100

    unused file deleted

--- src/Core/App/GoUpButton/index.js
+++
@@ -1,8 +0,0 @@
-import { StyledArrow } from "../../../generalTypes/Arrow"
-import { Button } from "./styled"
-
-export const GoUpButton = () => {
-  return(
-    <Button as="a" href="#search"><StyledArrow rotate big/></Button>
-  )
-}
