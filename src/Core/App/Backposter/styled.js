d0aa68fb997539277d1b8b58a5cb7695c51779a5
palel <32599663+palel@users.noreply.github.com>
2022-07-17 23:57:32 +0200

    poster styles correction

--- src/Core/App/Backposter/styled.js
+++ src/Core/App/Backposter/styled.js
@@ -1,7 +1,32 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Wrapper = styled.div`
   background: ${({ theme }) => theme.color.black};
+  margin-top: -28px;
+
+  width: 100%;
+  display: table;
+  overflow: hidden;
+  transition: all 1s ease-in-out;
+
+  ${({ hidden }) => hidden && css`
+    height: 0;
+    opacity: 0;
+    display: block;
+  `}
+`;
+
+export const ToggleHideButton = styled.button`
+  display: block ;
+  background: none;
+  padding: 10px;
+  margin-left: auto;
+  margin-right: 20px;
+  border: none;
+  
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export const Image = styled.div`
@@ -9,8 +34,9 @@ export const Image = styled.div`
   height: 800px;
   background-size: cover;
   background-position: 0% 30%;
+  margin-top: -10px;
   margin-bottom: 64px;
-
+  
   @media(max-width: ${({ theme }) => theme.breakpoint.laptopMax}) {
     height: 55vw;
     margin-bottom: 56px;
@@ -27,7 +53,8 @@ export const Vignette = styled.div`
   height: 100%;
   justify-content: flex-end;
   flex-wrap: wrap;
-  background: linear-gradient(270deg, #000000 5%, 
+  background: 
+    linear-gradient(270deg, #000000 5%, 
       rgba(0, 0, 0, 0.873268) 7%, 
       rgba(0, 0, 0, 0.720529) 8%, 
       rgba(0, 0, 0, 0.294577) 10%, 
@@ -44,13 +71,11 @@ export const Vignette = styled.div`
       rgba(0, 0, 0, 0.439033) 9.34%, 
       rgba(0, 0, 0, 0.20628) 15.16%, 
       rgba(0, 0, 0, 0) 24.22%), 
-    linear-gradient(189.44deg, 
-      rgba(0, 0, 0, 0) 58.48%, 
-      rgba(0, 0, 0, 0.106473) 63.17%, 
-      rgba(0, 0, 0, 0.235359) 68.85%, 
-      rgba(0, 0, 0, 0.492821) 78.08%, 
-      rgba(0, 0, 0, 0.740286) 85.86%,
-       #000000 92.87%);
+    linear-gradient(0deg, #000000 0%, 
+      rgba(0, 0, 0, 0.689555) 7%, 
+      rgba(0, 0, 0, 0.439033) 12%, 
+      rgba(0, 0, 0, 0.20628) 18%, 
+      rgba(0, 0, 0, 0) 25%);
 
   @media(max-width: ${({ theme }) => theme.breakpoint.laptopMax}) {
     padding: 8px;
