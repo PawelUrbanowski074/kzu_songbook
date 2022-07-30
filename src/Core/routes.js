6971757995fe654873e6c0c728529be26062bb8f
palel <32599663+palel@users.noreply.github.com>
2022-07-30 21:15:55 +0200

    routes for song  lists added

--- src/Core/routes.js
+++ src/Core/routes.js
@@ -1,3 +1,9 @@
 export const toSongs = () => "/utwory";
 
 export const toSong = ({ id } = { id: ":id" }) => `/utwory/${id}`;
+
+export const toSavedLists = () => "/listy";
+
+export const toList = ({ id } = { id: ":id" }) => `/listy/${id}`;
+
+export const toNewList = () => "/nowa";
